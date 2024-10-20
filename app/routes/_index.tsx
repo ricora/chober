411174929b6fa3a3d8f93424d6eadfd4902e747f
BruCandy <161817534+BruCandy@users.noreply.github.com>
2024-10-20 15:31:13 +0900

    ホーム画面に売り上げの合計金額を表示する (#14)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,67 @@
-import { Box, HStack, VStack } from "@chakra-ui/react"
+import { Box, HStack, Text, VStack } from "@chakra-ui/react"
+import { json, useLoaderData } from "@remix-run/react"
+import { useEffect, useState } from "react"
+import { readDetail } from "~/crud/crud_details"
+import { readProduct } from "~/crud/crud_products"
 
 export default function Home() {
-  return (
-    <div>
-      <HStack>
-        <Box w="450px" h="500px" bg="white">
-          <Box fontSize="xl">総売り上げ</Box>
-          <VStack>
-            <Box fontSize="xl">5000円</Box>
-          </VStack>
-        </Box>
-      </HStack>
-    </div>
+  const { details: details, products: products } =
+    useLoaderData<typeof loader>()
+  const [loadTime, setLoadTime] = useState<Date | null>(null)
+
+  const totalProfit = details.reduce(
+    (sum, detail) =>
+      sum +
+      detail.quantity *
+        products
+          .filter((product) => detail.product_id === product.product_id)
+          .reduce((sum, product) => sum + product.price, 0),
+    0,
   )
+
+  useEffect(() => {
+    const time: Date = new Date()
+    setLoadTime(time)
+  }, [])
+
+  return (
+    <VStack>
+      <Box
+        w={{ md: "100%", lg: "975px" }}
+        h="280px"
+        bg="white"
+        borderRadius="10px"
+        shadow="md"
+        p={4}
+      >
+        <VStack>
+          <HStack alignItems="baseline">
+            <Text fontSize="3xl">総額</Text>
+            <Text
+              color="red"
+              fontFamily="'Playfair Display', serif"
+              fontSize="8xl"
+              fontStyle="italic"
+            >
+              {totalProfit}
+            </Text>
+            <Text fontSize="3xl">円</Text>
+          </HStack>
+          <Text fontSize="3xl">
+            の売り上げ（
+            {loadTime ? loadTime.toLocaleString() : "ロード中"}時点）
+          </Text>
+        </VStack>
+      </Box>
+    </VStack>
+  )
+}
+
+export const loader = async () => {
+  const response_details = await readDetail()
+  const response_products = await readProduct()
+  return json({
+    details: response_details,
+    products: response_products,
+  })
 }
