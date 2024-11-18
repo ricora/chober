675f92148121d55a9904a0d068b73029063aec29
rai <96561881+r4ai@users.noreply.github.com>
2024-11-18 18:00:29 +0900

    fix: デザインを既存のものに合わせる

--- app/components/organisms/register/ProductForm.tsx
+++ app/components/organisms/register/ProductForm.tsx
@@ -2,7 +2,7 @@ import { Form } from "@remix-run/react"
 import { FormApi, ReactFormApi } from "@tanstack/react-form"
 import type { FC } from "react"
 import { TypeProduct } from "~/type/typeproduct"
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react"
+import { Button, FormControl, FormLabel, Input, VStack } from "@chakra-ui/react"
 import { FieldInfo } from "~/components/molecules/FieldInfo"
 
 export type ProductFormValues = Omit<TypeProduct, "product_id">
@@ -20,87 +20,92 @@ export const ProductForm: FC<ProductFormProps> = ({
 }) => {
   return (
     <Form method="post" onSubmit={() => form.handleSubmit()}>
-      <form.Field name="product_name">
-        {(field) => (
-          <FormControl isInvalid={field.state.meta.errors.length > 0}>
-            <FormLabel>商品名</FormLabel>
-            <Input
-              type="text"
-              placeholder="焼きそば"
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
-            <FieldInfo field={field} />
-          </FormControl>
-        )}
-      </form.Field>
-      <form.Field name="price">
-        {(field) => (
-          <FormControl isInvalid={field.state.meta.errors.length > 0}>
-            <FormLabel>価格</FormLabel>
-            <Input
-              type="number"
-              placeholder="500"
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.valueAsNumber)}
-            />
-            <FieldInfo field={field} />
-          </FormControl>
-        )}
-      </form.Field>
-      <form.Field name="stock">
-        {(field) => (
-          <FormControl isInvalid={field.state.meta.errors.length > 0}>
-            <FormLabel>在庫</FormLabel>
-            <Input
-              type="number"
-              placeholder="100"
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.valueAsNumber)}
-            />
-            <FieldInfo field={field} />
-          </FormControl>
-        )}
-      </form.Field>
-      <form.Field name="image">
-        {(field) => (
-          <FormControl isInvalid={field.state.meta.errors.length > 0}>
-            <FormLabel>商品画像</FormLabel>
-            <Input
-              type="text"
-              placeholder="https://example.com/image.jpg"
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
-            <FieldInfo
-              field={field}
-              helperText="商品画像は画像URLで入力してください"
-            />
-          </FormControl>
-        )}
-      </form.Field>
-      <form.Subscribe
-        selector={(formState) => [formState.canSubmit, formState.isSubmitting]}
-      >
-        {([canSubmit, isSubmitting]) => (
-          <Button
-            type="submit"
-            isLoading={isSubmitting}
-            loadingText={submittingText}
-            disabled={!canSubmit}
-          >
-            {submitText}
-          </Button>
-        )}
-      </form.Subscribe>
+      <VStack gap={4}>
+        <form.Field name="product_name">
+          {(field) => (
+            <FormControl isInvalid={field.state.meta.errors.length > 0}>
+              <FormLabel>商品名</FormLabel>
+              <Input
+                type="text"
+                placeholder="焼きそば"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              <FieldInfo field={field} />
+            </FormControl>
+          )}
+        </form.Field>
+        <form.Field name="price">
+          {(field) => (
+            <FormControl isInvalid={field.state.meta.errors.length > 0}>
+              <FormLabel>価格</FormLabel>
+              <Input
+                type="number"
+                placeholder="500"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+              />
+              <FieldInfo field={field} />
+            </FormControl>
+          )}
+        </form.Field>
+        <form.Field name="stock">
+          {(field) => (
+            <FormControl isInvalid={field.state.meta.errors.length > 0}>
+              <FormLabel>在庫</FormLabel>
+              <Input
+                type="number"
+                placeholder="100"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+              />
+              <FieldInfo field={field} />
+            </FormControl>
+          )}
+        </form.Field>
+        <form.Field name="image">
+          {(field) => (
+            <FormControl isInvalid={field.state.meta.errors.length > 0}>
+              <FormLabel>商品画像</FormLabel>
+              <Input
+                type="text"
+                placeholder="https://example.com/image.jpg"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              <FieldInfo
+                field={field}
+                helperText="商品画像は画像URLで入力してください"
+              />
+            </FormControl>
+          )}
+        </form.Field>
+        <form.Subscribe
+          selector={(formState) => [
+            formState.canSubmit,
+            formState.isSubmitting,
+          ]}
+        >
+          {([canSubmit, isSubmitting]) => (
+            <Button
+              type="submit"
+              isLoading={isSubmitting}
+              loadingText={submittingText}
+              disabled={!canSubmit}
+            >
+              {submitText}
+            </Button>
+          )}
+        </form.Subscribe>
+      </VStack>
     </Form>
   )
 }
