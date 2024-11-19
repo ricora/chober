f8c7b1c2e5d5b8d030ec302332d0d6f7a9f426c7
rai <96561881+r4ai@users.noreply.github.com>
2024-11-19 19:32:10 +0900

    chore: Lintエラーを解決するために、TypeScriptではreact/prop-typesルールを無効化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -71,6 +71,9 @@ module.exports = {
         "plugin:import/recommended",
         "plugin:import/typescript",
       ],
+      rules: {
+        "react/prop-types": "off",
+      },
     },
 
     // Node
