a7ca3aa2989e48d22c948ba61c14e4a0a9f3238f
CandyCandyCandy <161817534+BruCandy@users.noreply.github.com>
2024-11-21 23:56:43 +0900

    resolved lint error and format error

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 export default defineConfig({
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "app"),
+      "~": path.resolve("app"),
     },
   },
   test: {
